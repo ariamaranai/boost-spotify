8d8022860b74d2628727ad59ba4c28137aa3c3de
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2024-12-13 15:35:18 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,54 +1,73 @@
 {
-let o = Object, k = {}, e = document.createElement("b"), f =v=> {
-  let n = v.playcount || ""
-  v.playcount =
-   (o = n.length) < 4 ? n :
-    o < 7 ? n.slice(0, o - 3) + "," + n.slice(-3) :
-    o < 10 ? n.slice(0, o - 6) + "," + n.slice(-6, -3) + "," + n.slice(-3) :
-    (+n).toLocaleString()
-  return v
-}
-HTMLDivElement.prototype.setAttribute = new Proxy(e.setAttribute, {
-  apply: (a, b, c)=> {
-    switch (c[0]) {
-      case "class":
-        b.className = c[1]
-        break
-      case "id":
-        b.id = c[1]
-        break
-      case "aria-label":
-      case "aria-labelledby":
-      case "dir":
-      case "draggable":
-      case "lang":
-      case "role":
-      case "tabindex":
-      case "title":
-        break
-      case "data-testid":
-        Reflect.apply(a, b, c),
-        c[1] == "tracklist-row" && (c = k[location.pathname.slice(-22)]) && (
-          a = b.querySelector("a").href.slice(-22),
-          (c = c.find(v=> v?.uri?.slice(-22) == a)?.playcount) &&
-          ((b = b.lastChild).insertBefore(e.cloneNode(), b.firstChild).textContent = c)
-        )
-        break
-      default:
-        Reflect.apply(a, b, c)
+  let o = Object;
+  let buf = {};
+  let e = document.createElement("b");
+  e.className = "encore-text-body-small HxDMwNr5oCxTOyqt85gi";
+  let toLocale = v => {
+    let n = v.playcount || "";
+    let l = n.length;
+    v.playcount =
+      l < 4
+      ? n
+      : l < 7
+      ? n.slice(0, l - 3) + "," + n.slice(-3)
+        : l < 10
+        ? n.slice(0, l - 6) + "," + n.slice(-6, -3) + "," + n.slice(-3)
+        : (+n).toLocaleString();
+    return v;
+  };
+  HTMLDivElement.prototype.setAttribute = new Proxy(e.setAttribute, {
+    apply: (a, b, c) => {
+      switch (c[0]) {
+        case "class":
+          b.className = c[1];
+          break;
+        case "id":
+          b.id = c[1];
+          break;
+        case "aria-label":
+        case "aria-labelledby":
+        case "dir":
+        case "draggable":
+        case "lang":
+        case "role":
+        case "tabindex":
+        case "title":
+          break;
+        case "data-testid":
+          Reflect.apply(a, b, c);
+          if (c[1] == "tracklist-row") {
+            let items = buf[location.pathname.slice(-22)];
+            if (items) {
+              let uri = b.querySelector("a").href.slice(-22);
+              let n = items.find(v => v?.uri?.slice(-22) == uri)?.playcount;
+              if (n) {
+                let parent = b.lastChild;
+                parent.insertBefore(e.cloneNode(), parent.firstChild).textContent = n;
+              } 
+            }
+          }
+          break;
+        default:
+          Reflect.apply(a, b, c);
+      }
+    },
+  });
+  o.freeze = o.seal = a => a;
+  o.isFrozen = o.isSealed = () => 0;
+  (o = Response.prototype).json = new Proxy(o.json, {
+    apply: async (a, b, c) => {
+      let result = await Reflect.apply(a, b, c);
+      let data = result.data;
+      let items =
+        data?.albumUnion?.tracksV2?.items?.map(v => toLocale(v?.track))
+        ?? data?.playlistV2?.content?.items?.map(v => toLocale(v?.itemV2?.data));
+      if (items) {
+        let uri = location.pathname.slice(-22);
+        buf[uri] = buf[uri] ? buf[uri].concat(items) : items; 
+      }
+      return result;
     }
-  }
-})
-o.freeze = o.seal =a=> a,
-o.isFrozen = o.isSealed =()=> 0,
-(o=Response.prototype).json = new Proxy(o.json, {
-  apply: async (a, b, c)=> (
-    (a = (b = (c = await Reflect.apply(a, b, c)).data)?.albumUnion?.tracksV2?.items?.map(v=> f(v?.track)) ??
-     b?.playlistV2?.content?.items?.map(v=> f(v?.itemV2?.data))) &&
-    (k[b=location.pathname.slice(-22)] = k[b] ? k[b].concat(a) : a),
-    c
-  )
-})
-e.className = "encore-text-body-small HxDMwNr5oCxTOyqt85gi"
+  });
 }
-isFinite = Number.isFinite =()=> 1
+isFinite = Number.isFinite = () => 1;
