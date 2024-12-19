9b16391d373c38c561bd8d62d46fb8c5732c8a27
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2024-12-19 23:27:58 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
 XMLHttpRequest = 0;
 isFinite =
 Number.isFinite =
-Object.hasOwnProperty =() => 1;
+Object.hasOwn =
+Object.hasOwnProperty = () => 1;
 {
   let o = Object;
-  let buf = {};
+  o.freeze = o.seal = a => a;
+  o.isFrozen = o.isSealed = () => 0;
+
   let e = document.createElement("b");
   e.className = "encore-text-body-small HxDMwNr5oCxTOyqt85gi";
+
+  let buf = {};
   let toLocale = v => {
     let n = v.playcount || "";
     let l = n.length;
@@ -30,14 +35,13 @@ Object.hasOwnProperty =() => 1;
       ? fet(a, b)
       : Promise.reject(res);
 
-  HTMLDivElement.prototype.setAttribute = new Proxy(e.setAttribute, {
-    apply: (a, b, c) => {
-      switch (c[0]) {
-        case "class":
-          b.className = c[1];
-          break;
+  HTMLDivElement.prototype.setAttribute = function (a, b) {
+    switch (a) {
+      case "class":
+        this.className = b;
+        break;
         case "id":
-          b.id = c[1];
+          this.id = b;
           break;
         case "aria-label":
         case "aria-labelledby":
@@ -49,38 +53,33 @@ Object.hasOwnProperty =() => 1;
         case "title":
           break;
         case "data-testid":
-          Reflect.apply(a, b, c);
-          if (c[1] == "tracklist-row") {
+          if (b == "tracklist-row") {
             let items = buf[location.pathname.slice(-22)];
             if (items) {
-              let uri = b.querySelector("a").href.slice(-22);
+              let uri = this.querySelector("a").href.slice(-22);
               let n = items.find(v => v?.uri?.slice(-22) == uri)?.playcount;
               if (n) {
-                let parent = b.lastChild;
+                let parent = this.lastChild;
                 parent.insertBefore(e.cloneNode(), parent.firstChild).textContent = n;
               } 
             }
           }
-          break;
         default:
-          Reflect.apply(a, b, c);
+          Element.prototype.setAttribute.call(this, a, b);
       }
-    },
-  });
-  o.freeze = o.seal = a => a;
-  o.isFrozen = o.isSealed = () => 0;
-  (o = Response.prototype).json = new Proxy(o.json, {
-    apply: async (a, b, c) => {
-      let result = await Reflect.apply(a, b, c);
-      let data = result.data;
-      let items =
-        data?.albumUnion?.tracksV2?.items?.map(v => toLocale(v?.track))
+  };
+
+  o = Response.prototype.json;
+  Response.prototype.json = async function (a, b) {
+    let result = await o.call(this, a, b);
+    let data = result.data;
+    let items =
+      data?.albumUnion?.tracksV2?.items?.map(v => toLocale(v?.track))
         ?? data?.playlistV2?.content?.items?.map(v => toLocale(v?.itemV2?.data));
-      if (items) {
-        let uri = location.pathname.slice(-22);
-        buf[uri] = buf[uri] ? buf[uri].concat(items) : items; 
-      }
-      return result;
+    if (items) {
+      let uri = location.pathname.slice(-22);
+      buf[uri] = buf[uri] ? buf[uri].concat(items) : items; 
     }
-  });
+    return result;
+  };
 }
