3e090b38c6d3ae0afee87cb244934875b06e3067
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2025-02-21 12:58:47 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,26 @@
 XMLHttpRequest = 0;
 {
-  let o = Object;
-  let p = Element.prototype;
-  let d = document;
-  let setter = { set: () => 0 };
+  let
+  o = Object,
+  p = Element.prototype,
+  setter = { set: () => 0 },
+  be = document.createElement("dt"),
+  buf = new Map,
+  toLocale = v => {
+    let n = v.playcount || "", l = n.length;
+    v.playcount =
+      l < 4 ? n
+    : l < 7 ? n.slice(0, l - 3) + "," + n.slice(-3)
+    : l < 10 ? n.slice(0, l - 6) + "," + n.slice(-6, -3) + "," + n.slice(-3)
+    : (+n).toLocaleString();
+    return v;
+  },
+  fet = fetch,
+  dummyThen = {
+    then: ()=> dummyThen,
+    catch: () => dummyThen
+  },
+  test = RegExp.prototype.test;
 
   o.freeze =
   o.seal = a => a;
@@ -26,36 +43,13 @@ XMLHttpRequest = 0;
     timeout: setter
   });
 
-  HTMLBodyElement.prototype.appendChild = a =>
-    a.id == "ad-tracking-pixel" ? 0 :
-    a.async ? (HTMLBodyElement.prototype.appendChild = Node.prototype.appendChild, 0)
-            : Node.prototype.appendChild.call(d.body, a);
-
+  HTMLBodyElement.prototype.appendChild = a => a.id == "ad-tracking-pixel" ? 0 :
+    a.async
+      ? (HTMLBodyElement.prototype.appendChild = Node.prototype.appendChild, 0)
+      : Node.prototype.appendChild.call(document.body, a);
             
-  let createElement = d.createElement.bind(d);
-  let ae = createElement("a");
-  let be = createElement("b");
-  ae.setAttribute("style", "display:contents");
-  be.className = "encore-text-body-small HxDMwNr5oCxTOyqt85gi";
-  d.createElement = a => a == "a" ? ae.cloneNode() : createElement(a);
+  be.setAttribute("style", "font-size:14px");
 
-  let buf = new Map;
-  let toLocale = v => {
-    let n = v.playcount || "";
-    let l = n.length;
-    v.playcount =
-      l < 4 ? n
-    : l < 7 ? n.slice(0, l - 3) + "," + n.slice(-3)
-    : l < 10 ? n.slice(0, l - 6) + "," + n.slice(-6, -3) + "," + n.slice(-3)
-    : (+n).toLocaleString();
-    return v;
-  }
-
-  let fet = fetch;
-  let dummyThen = {
-    then: ()=> dummyThen,
-    catch: () => dummyThen
-  };
   fetch = (a, b) =>
     a != "https://gae2-spclient.spotify.com/gabo-receiver-service/public/v3/events" &&
     a != "https://gae2-spclient.spotify.com/melody/v1/msg/batch" &&
@@ -76,14 +70,11 @@ XMLHttpRequest = 0;
       case "dragenter":
       case "dragexit":
       case "dragleave":
-      case "dragover":
-      case "dragstart":
-      case "drop":
+   // case "dragover":
+   // case "dragstart":
+   // case "drop":
       case "error":
       case "gotpointercapture":
-      case "keydown":
-      case "keypress":
-      case "keyup":
       case "lostpointercapture":
       case "mouseenter":
       case "mouseleave":
@@ -110,49 +101,25 @@ XMLHttpRequest = 0;
         return EventTarget.prototype.addEventListener.call(this, a, b, c);
     }
   }
-  let setAttr = p.setAttribute;
-  p.setAttribute = function (a, b) {
-    switch (a) {
-   // case "aria-colcount":
-   // case "aria-colindex":
-      case "aria-live":
-   // case "aria-rowcount":
-   // case "aria-rowindex":
-   // case "aria-setsize":
-      case "aria-sort":
-      case "data-animation":
-      case "data-encore-id":
-   // case "data-overlayscrollbars":
-   // case "data-overlayscrollbars-viewport":
-      case "data-theme":
-      case "data-webpack":
-      case "draggable":
-      case "lang":
-        break;
-      case "height":
-      case "hidden":
-      case "href":
-      case "id":
-      case "max":
-      case "min":
-      case "step":
-      case "src": 
-      case "width":
-        this[a] = b;
-        break;
-      case "data-testid":
-        if (b == "tracklist-row" && this.tagName == "DIV") {
-          let items = buf[location.pathname.slice(-22)];
-          if (items) {
-            let u = this.querySelector("a").href.slice(-22);
-            let n = items.find(v => v?.uri?.slice(-22) == u)?.playcount;
-            n && ((u = this.lastChild).insertBefore(be.cloneNode(), u.firstChild).textContent = n)
-          }
-        }
-        break;
-      default:
-        setAttr.call(this, a, b);
+  /*
+    case "aria-colcount":
+    case "aria-colindex":
+    case "data-overlayscrollbars":
+    case "data-overlayscrollbars-viewport":
+    case "aria-rowcount":
+    case "aria-rowindex":
+    case "aria-setsize":
+  */
+  HTMLDivElement.prototype.setAttribute = function (a, b) {
+    if (b == "tracklist-row") {
+      let items = buf[location.pathname.slice(-22)];
+      if (items) {
+        let u = this.querySelector("a").href.slice(-22);
+        let n = items.find(v => v?.uri?.slice(-22) == u)?.playcount;
+        n && ((u = this.lastChild).insertBefore(
+          be.cloneNode(), u.firstChild).textContent = n)
       }
+    } else Element.prototype.setAttribute.call(this, a, b);
   }
   let hasOwnProperty = (p = o.prototype).hasOwnProperty;
   p.hasOwnProperty = function (a) {
@@ -160,6 +127,7 @@ XMLHttpRequest = 0;
       case "adsCoreConnector":
       case "adsEnabled":
       case "alt":
+      case "alternativeText":
       case "aria-activedescendant":
       case "aria-checked":
       case "aria-controls":
@@ -168,6 +136,7 @@ XMLHttpRequest = 0;
       case "aria-expanded":
       case "aria-haspopup":
       case "aria-hidden":
+      case "aria-invalid":
       case "aria-label":
       case "aria-labelledby":
       case "aria-level":
@@ -177,9 +146,20 @@ XMLHttpRequest = 0;
       case "aria-pressed":
       case "aria-selected":
       case "aria-valuetext":
+      case "ariaDescribedBy":
       case "ariaHidden":
+      case "ariaHideApp":
+      case "ariaLabel":
+      case "ariaLabelledBy":
       case "ariaPauseLabel":
       case "ariaPlayLabel":
+      case "backgroundColor":
+      case "backgroundImage":
+      case "backgroundImageUrl":
+      case "backgroundImages":
+      case "backgroundPosition":
+      case "backgroundRepeat":
+      case "backgroundSize":
       case "borderRadius":
       case "data-is-icon-only":
       case "data-right-sidebar-hidden":
@@ -188,36 +168,42 @@ XMLHttpRequest = 0;
       case "dateTime":
       case "dir":
       case "disabled":
+      case "error":
+      case "errorMessage":
+      case "errorName":
+      case "hover":
+      case "hoverAnimationDuration":
+      case "hoverBackgroundColor":
       case "isDownloadable":
       case "isOffline":
       case "isPremium":
       case "line-clamp":
       case "lineClamp":
+      case "loadOffline":
       case "loading":
       case "onDoubleClick":
       case "onError":
       case "onTouchEnd":
       case "onTouchStart":
+      case "placeholder":
+      case "playAriaLabel":
       case "referrer":
-      case "role":
+   // case "role":
       case "shiftKey":
+      case "showDelay":
       case "spellCheck":
       case "tabIndex":
-      case "title":
+      case "tabindex":
+      case "theme":
+   // case "title":
         return 0;
       case "isDesktop":
+      case "isWebPSupported":
         return 1;
       default:
         // typeof a == "string" && a.length > 2 && (kkk[a] ? ++kkk[a] : kkk[a] = 1);
         return hasOwnProperty.call(this, a);
     }
-  }
-
-  let test = (p = RegExp.prototype).test;
-  p.test = function (a) {
-    return typeof a == "function" || a != " " && // ua
-      a == "@webgate/gabo-receiver-service/public/v3/events" ||
-      test.call(this, a)
   }
 
   o = (p = Response.prototype).json;
@@ -233,5 +219,11 @@ XMLHttpRequest = 0;
       buf[uri] = itemBuf ? itemBuf.concat(items) : items;
     }
     return result;
+  }
+
+  RegExp.prototype.test = function (a) {
+    return typeof a == "function" || a != " " && // ua
+      a == "@webgate/gabo-receiver-service/public/v3/events" ||
+      test.call(this, a)
   }
 }
